b7b385e185737407bc7acc643488a2701b6ed177
itsrajaniket <itsrajaniket@gmail.com>
2025-12-25 01:48:38 +0530

    Added solution for Question 4

---
+++ coding_practice/4.js
@@ -0,0 +1,11 @@
+//4=> Write a program to print the name and city {Ravi lives in Delhi}
+
+let person = {
+  name: "Ravi",
+  age: 25,
+  city: "Delhi",
+};
+// way 1
+console.log(person.name + " lives in " + person.city);
+// way 2
+console.log(`${person.name} lives in ${person.city}`);
