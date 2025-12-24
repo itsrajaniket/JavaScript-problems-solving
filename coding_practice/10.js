59d0e5b8e0dd8e1a342aa8c3f1403d1d38c66953
itsrajaniket <itsrajaniket@gmail.com>
2025-12-25 01:48:39 +0530

    Added solution for Question 10

---
+++ coding_practice/10.js
@@ -0,0 +1,10 @@
+// 🔟 Count Digits in a Number Given 12345, count how many digits it has.
+
+//  way 1
+let num6 = 123456;
+let num3 = num6 / 10;
+
+// const num7 = num6.toString();
+// console.log(num7.length);
+
+console.log(num3);
