235efc31c930ed0c1ceab7e83a43fe1c2407a7e6
itsrajaniket <itsrajaniket@gmail.com>
2026-01-07 06:01:25 +0530

    Added question 28 file

--- 28.js
+++ 28.js
@@ -1,8 +1,6 @@
+let arr = [2, 3, 4, 5, 6, 8, 431, 542, 23545, 46];
 
-let arr = [2,3,4,5,6,8,431,542,23545,46]
-
-for (let i = 0 ; i < arr.length ; i++){
-    arr[i] = -arr[i];
-
+for (let i = 0; i < arr.length; i++) {
+  arr[i] = -arr[i];
 }
-console.log(arr)
+console.log(arr);
