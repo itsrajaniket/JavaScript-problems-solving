30596ced3f8ee1ed83f4ffa32d18360e717eb104
itsrajaniket <itsrajaniket@gmail.com>
2026-01-07 06:25:00 +0530

    Removed duplicate JS files from root, keeping all inside coding_practice folder

--- 14.js
+++
@@ -1,2 +0,0 @@
-// 14️⃣ Count Even Numbers in an Array
-// [1, 2, 3, 4, 6]
--- 8.js
+++
@@ -1,9 +0,0 @@
-// 8️⃣ Sum of Numbers from 1 to 1000000
-
-let sum4 = 0;
-for (let i = 0; i <= 1000000; i++) {
-  sum4 = sum4 + i;
-}
-console.log(sum4);
-
-console.log("next qustion");
