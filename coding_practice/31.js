2f101a1206a86ac6ed1cf17a8df390581dd449f5
itsrajaniket <itsrajaniket@gmail.com>
2026-01-10 20:14:21 +0530

    Added question 31

---
+++ coding_practice/31.js
@@ -0,0 +1,14 @@
+// Write a function that takes a string s and returns the string reversed.
+
+function reverseString(s) {
+  let reversed = "";
+  for (let i = s.length - 1; i >= 0; i--) {
+    reversed += s[i];
+    console.log(reversed);
+  }
+  return reversed;
+}
+
+// Test
+console.log(reverseString("hello")); // "olleh"
+console.log(reverseString("aniket")); // "tekina"
