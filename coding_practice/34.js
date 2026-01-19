51af2ca4d4364aa4def502208c1d3a3b8f8c0be5
itsrajaniket <itsrajaniket@gmail.com>
2026-01-18 21:46:02 -1000

    34 Find the First Word Length

---
+++ coding_practice/34.js
@@ -0,0 +1,18 @@
+// Find the First Word Length
+
+function firstWordLength(s) {
+  let result = s.trimStart();
+
+  let count = 0;
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] !== " ") {
+      count += 1;
+    } else break;
+  }
+
+  return count;
+}
+console.log(firstWordLength("Hello World")); // 5
+console.log(firstWordLength("  fly me   to   the moon")); // 3 ("fly")
+console.log(firstWordLength("a")); // 1
+console.log(firstWordLength("a ")); // 1
