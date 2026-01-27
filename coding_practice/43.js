bcfdde8342106c5af7d88f709fd40e9118b8b81e
itsrajaniket <itsrajaniket@gmail.com>
2026-01-28 02:06:02 +0530

    all star pattern

--- coding_practice/43.js
+++ coding_practice/43.js
@@ -5,3 +5,38 @@ for (let i = 0; i < 6; i++) {
   }
   console.log(row);
 }
+for (let i = 0; i < 6; i++) {
+  let row = " ";
+  for (let i = 0; i < 6; i++) {
+    row = row + "*" + " " + " ";
+  }
+  console.log(row);
+}
+
+let row1 = " ";
+for (let i = 0; i < 6; i++) {
+  row1 = row1 + "*" + " " + " ";
+  console.log(row1);
+}
+
+let row = " ";
+for (let i = 6; i > 0; i--) {
+  row = row + "*" + " " + " ";
+  console.log(row);
+}
+
+for (let i = 0; i < 5; i++) {
+  row = " ";
+  for (let k = 1; k < i + 2; k++) {
+    row = row + k;
+  }
+  console.log(row);
+}
+
+for (let i = 0; i < 5; i++) {
+  row = " ";
+  for (let k = 1; k < i + 2; k++) {
+    row = row + (i + 1);
+  }
+  console.log(row);
+}
