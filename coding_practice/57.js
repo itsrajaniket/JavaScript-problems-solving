46c3ad6133471aecc9a9858f6d13368db427a485
itsrajaniket <itsrajaniket@gmail.com>
2026-02-19 23:09:01 +0530

    new code

--- coding_practice/57.js
+++ coding_practice/57.js
@@ -11,4 +11,4 @@ console.log(mySet);
 
 const numbers = [1, 2, 2, 3, 4, 4, 6];
 const uniqueNumbers = [...new Set(numbers)];
-console.log(uniqueNumbers); // [1, 2, 3, 4]
+console.log(uniqueNumbers); // [1, 2, 3, 4 ,5]
