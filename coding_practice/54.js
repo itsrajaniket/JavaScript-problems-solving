9fbca2b8c0aee192f968dae816b247fee7f28750
itsrajaniket <itsrajaniket@gmail.com>
2026-02-21 10:30:00 +0530

    commit for Feb 21

--- coding_practice/54.js
+++ coding_practice/54.js
@@ -12,6 +12,6 @@ function missingNumber(nums) {
   return expectedSum - actualSum;
 }
 
-console.log(missingNumber([3, 0, 1])); // 2
+console.log(missingNumber([3, 0, 4, 1])); // 2
 console.log(missingNumber([0, 1])); // 2
 console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1, 9])); // 8
