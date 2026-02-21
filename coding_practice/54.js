2c59c1d0862a8d53487fc40fa45ea83b220db4d7
itsrajaniket <itsrajaniket@gmail.com>
2026-02-21 10:30:00 +0530

    Feb 21

--- coding_practice/54.js
+++ coding_practice/54.js
@@ -14,4 +14,4 @@ function missingNumber(nums) {
 
 console.log(missingNumber([3, 0, 1])); // 2
 console.log(missingNumber([0, 1])); // 2
-console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])); // 8
+console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1, 9])); // 8
