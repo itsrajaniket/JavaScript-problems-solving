20018df7c025e64f76b4654a4521a2aa07f98266
itsrajaniket <itsrajaniket@gmail.com>
2026-04-03 21:33:10 +0530

    Add function to find length of longest substring without repeating characters

---
+++ coding_practice/61.js
@@ -0,0 +1,22 @@
+// Given a string s, find the length of the longest substring without repeating characters.
+
+function lengthOfLongestSubstring(s) {
+  let set = new Set();
+  let left = 0,
+    maxLength = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    while (set.has(s[right])) {
+      set.delete(s[left]);
+      left++;
+    }
+    set.add(s[right]);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+}
+
+console.log(lengthOfLongestSubstring("abcabcbb")); // 3
+console.log(lengthOfLongestSubstring("bbbbb")); // 1
+console.log(lengthOfLongestSubstring("pwwkew")); // 3
